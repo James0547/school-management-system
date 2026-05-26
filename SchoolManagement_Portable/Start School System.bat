98dcbda5040ad69a5b8362b7cdd5e825a81620c7
HeroHero <jamesquaicoeever@gmail.com>
2026-05-26 21:56:37 +0000

    Initial commit: Complete School Management System

--- SchoolManagement_Portable/Start School System.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title School Management System
-color 0A
-echo Starting School Management System...
-echo.
-start "" "SchoolManagement.exe"
-echo Application started! Check your browser.
-echo.
-pause
